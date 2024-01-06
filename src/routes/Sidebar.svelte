bb94556a6e7bcf9e8e96f8c77a98df79957f8932
DrewPsi <Drewsteindl@icloud.com>
2024-01-06 16:31:35 -0500

    final touches

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -269,8 +269,21 @@
     ☰
 </button>
 {/if}
-
+<div class="signature">
+  Drew & Ryan
+</div>
 <style>
+  @import url('https://fonts.googleapis.com/css?family=Great+Vibes');
+      .signature {
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        color: rgba(192, 192, 192, 0.61); /* Translucent gray */
+        font-family: 'Great Vibes', cursive; /* Specify the imported cursive font */
+        padding: 10px;
+        font-size: 1.5em;
+        user-select: none;
+    }
     .input-container1 {
         display: flex;
         align-items: center;
